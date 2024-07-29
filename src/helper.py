926609e5027e05739cc21ce080bb81e895a1d117
Olawale Ogundeji <abiodungndj@gmail.com>
2024-07-29 16:42:32 +0100

    update files

--- src/helper.py
+++ src/helper.py
@@ -1,14 +1,15 @@
 import speech_recognition as sr
-import google.generativeai as palm
 import google.generativeai as genai
-from dotenv import load_dotenv
 import os
 from gtts import gTTS
+from dotenv import load_dotenv
 
-
+#GOOGLE_API_KEY = "****************************"
+# Load environment variables from .env file
 load_dotenv()
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  
-os.environ['GOOGLE_API_KEY'] =  GOOGLE_API_KEY
+
+# Get the API key from environment variable
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 def voice_input():
     # Create a recognizer instance
@@ -26,27 +27,13 @@ def voice_input():
         print("Sorry, could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
+    
 def text_to_speech(text):
     # Create a gTTS object
     tts = gTTS(text=text, lang='en')  # Language can be changed
 
     # Save the audio as an MP3 file
     tts.save("speech.mp3")
-
-# def llm_model_object(user_text):
-
-#     model = "models/text-bison-001"
-
-#     completion = palm.generate_text(
-#     model=model,
-#     prompt=user_text,
-#     temperature=0.3,
-#     # The maximum length of the response
-#     max_output_tokens=800,
-#     )
-
-#     return completion.result
 
 def llm_model_object(user_text):
     genai.configure(api_key=GOOGLE_API_KEY)
